af67295e5ac2ac858b842b379eced9b21bf6a9cf
Jorge Ruiz Aquino <jesfre.gy@gmail.com>
2019-05-15 06:00:20 -0400

    Created project as a Maven project. Added Spring Boot starter dependency. Added Lombok dependency.

---
+++ src/main/java/com/blogspot/jesfre/YourTunnelApplication.java
@@ -0,0 +1,13 @@
+package com.blogspot.jesfre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourTunnelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourTunnelApplication.class, args);
+	}
+
+}
